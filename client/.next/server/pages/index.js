b29b9a84d9173e67acdf7eba7242cf8c77d14719
abi amarulloh <abi@appinzpire.net>
2022-06-20 19:14:01 +0700

    update

---
+++ client/.next/server/pages/index.js
@@ -0,0 +1,66 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./layouts/footer.js":
+/*!***************************!*\
+  !*** ./layouts/footer.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ footer)\n/* harmony export */ });\nfunction footer() {\n    return \"ini footer\";\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9sYXlvdXRzL2Zvb3Rlci5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQWUsU0FBU0EsTUFBTSxHQUFHO0lBQzdCLE9BQU8sWUFBWTtDQUN0QiIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2xheW91dHMvZm9vdGVyLmpzP2ZiMTciXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gZm9vdGVyKCkge1xuICAgIHJldHVybiBcImluaSBmb290ZXJcIlxufSJdLCJuYW1lcyI6WyJmb290ZXIiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./layouts/footer.js\n");
+
+/***/ }),
+
+/***/ "./layouts/header.js":
+/*!***************************!*\
+  !*** ./layouts/header.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ header)\n/* harmony export */ });\nfunction header() {\n    return \"ini header\";\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9sYXlvdXRzL2hlYWRlci5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQWUsU0FBU0EsTUFBTSxHQUFHO0lBQzdCLE9BQU8sWUFBWTtDQUN0QiIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2xheW91dHMvaGVhZGVyLmpzP2ZhZmUiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gaGVhZGVyKCkge1xuICAgIHJldHVybiBcImluaSBoZWFkZXJcIlxufSJdLCJuYW1lcyI6WyJoZWFkZXIiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./layouts/header.js\n");
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _layouts_header__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../layouts/header */ \"./layouts/header.js\");\n/* harmony import */ var _layouts_footer__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../layouts/footer */ \"./layouts/footer.js\");\n\n\n\nfunction HomePage() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(_layouts_header__WEBPACK_IMPORTED_MODULE_1__[\"default\"], {}, void 0, false, {\n                fileName: \"/Users/abiamarulloh/Projects/Experimental-Projects/api-hadist/client/pages/index.js\",\n                lineNumber: 8,\n                columnNumber: 5\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n                children: \"Welcome to Next.js!\"\n            }, void 0, false, {\n                fileName: \"/Users/abiamarulloh/Projects/Experimental-Projects/api-hadist/client/pages/index.js\",\n                lineNumber: 9,\n                columnNumber: 5\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(_layouts_footer__WEBPACK_IMPORTED_MODULE_2__[\"default\"], {}, void 0, false, {\n                fileName: \"/Users/abiamarulloh/Projects/Experimental-Projects/api-hadist/client/pages/index.js\",\n                lineNumber: 10,\n                columnNumber: 5\n            }, this)\n        ]\n    }, void 0, true);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (HomePage);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBO0FBQXNDO0FBQ0E7QUFFdEMsU0FBU0UsUUFBUSxHQUFHO0lBQ2xCLHFCQUNFOzswQkFFQSw4REFBQ0YsdURBQU07Ozs7b0JBQVU7MEJBQ2pCLDhEQUFDRyxLQUFHOzBCQUFDLHFCQUFtQjs7Ozs7b0JBQU07MEJBQzlCLDhEQUFDRix1REFBTTs7OztvQkFBVTs7b0JBRWQsQ0FDTjtDQUNBO0FBRUQsaUVBQWVDLFFBQVEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9wYWdlcy9pbmRleC5qcz9iZWU3Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBIZWFkZXIgZnJvbSAnLi4vbGF5b3V0cy9oZWFkZXInXG5pbXBvcnQgRm9vdGVyIGZyb20gJy4uL2xheW91dHMvZm9vdGVyJ1xuXG5mdW5jdGlvbiBIb21lUGFnZSgpIHtcbiAgcmV0dXJuIChcbiAgICA8PlxuICAgIFxuICAgIDxIZWFkZXI+PC9IZWFkZXI+XG4gICAgPGRpdj5XZWxjb21lIHRvIE5leHQuanMhPC9kaXY+XG4gICAgPEZvb3Rlcj48L0Zvb3Rlcj5cbiAgICBcbiAgICA8Lz5cbilcbn1cblxuZXhwb3J0IGRlZmF1bHQgSG9tZVBhZ2UiXSwibmFtZXMiOlsiSGVhZGVyIiwiRm9vdGVyIiwiSG9tZVBhZ2UiLCJkaXYiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ client/pages/index.js
@@ -0,0 +1,16 @@
+import Footer from '../layouts/footer'
+import Header from '../layouts/header'
+
+function HomePage() {
+  return (
+    <>
+    <Header></Header>
+    
+    <div>Welcome to Next.js!</div>
+
+    <Footer></Footer>
+    </>
+)
+}
+
+export default HomePage
