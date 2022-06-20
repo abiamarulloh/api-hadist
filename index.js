f93e92ec93799d18d42a5f3b4e39f04b8f3c39a7
abi amarulloh <abi@appinzpire.net>
2022-06-21 06:51:31 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const shahih_muslim = require("./routes/shahih_muslim");
 const shahih_bukhari = require("./routes/shahih_bukhari");
 const sunan_tirmidzi = require("./routes/sunan_tirmidzi");
