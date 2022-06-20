f74d8356e3307a43e1e13dd15912496dd5e79c8c
abi amarulloh <abi@appinzpire.net>
2022-06-20 21:22:45 +0700

    update

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,24 @@
-import Footer from '../layouts/footer'
-import Header from '../layouts/header'
+import { useEffect, useState } from 'react';
+import Footer from '../layouts/footer';
+import Header from '../layouts/header';
 
 function HomePage() {
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [])
+
+  if (isLoading) return <p>Loading...</p>
+  if (!data) return <p>No Hadist data</p>
+
   return (
     <>
     <Header></Header>
