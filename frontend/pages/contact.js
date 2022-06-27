e4cc5dc7cb7c87d21d718495ff0fabce9a18dd0d
Sahal M <sahalmahfudh07@gmail.com>
2022-06-27 11:39:55 +0700

    Init Header, Profile, Contact Us

---
+++ frontend/pages/contact.js
@@ -0,0 +1,15 @@
+import styles from './index.module.css';
+
+function ProfilePage() {
+  return (
+    <>
+
+    <div className={styles.container}>
+      Ini Contact US
+    </div>
+
+    </>
+)
+}
+
+export default ProfilePage
