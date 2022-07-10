334cd16b69ff6a4f0e73fad5216d7a9b5f2ee2c9
abi amarulloh <--local>
2022-07-10 18:27:44 +0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const port = process.env.PORT || 3000;
 const shahih_muslim = require("./routes/shahih_muslim");
 const shahih_bukhari = require("./routes/shahih_bukhari");
 const sunan_tirmidzi = require("./routes/sunan_tirmidzi");
-const general_hadist = require("./routes/general");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger");
@@ -20,7 +19,6 @@ app.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use("/api/general", general_hadist);
 app.use("/api/hadist/shahih-muslim", shahih_muslim);
 app.use("/api/hadist/shahih-bukhari", shahih_bukhari);
 app.use("/api/hadist/sunan-tirmidzi", sunan_tirmidzi);
